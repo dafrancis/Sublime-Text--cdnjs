64dd1039095053a118a08694dcec648e9757a62b
Dafydd Francis <DaveOrdref@gmail.com>
2012-03-17 18:44:13 +0000

    Initial commit

---
+++ cdnjs.py
@@ -0,0 +1,44 @@
+import sublime
+import sublime_plugin
+import urllib
+import urllib2
+import threading
+import re
+import json
+import random
+
+class CdnjsCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.edit = edit
+		self.packages = CdnjsApiCall(30).run()
+		self.package_names = map(lambda x: x['name'], self.packages)
+		self.view.window().show_quick_panel(self.package_names, self.insert_tag)
+
+	def insert_tag(self, index):
+		package = self.packages[index]
+		cdn_url = "http://cdnjs.cloudflare.com/ajax/libs/"
+		path = "%s/%s/%s" % (package['name'], package['version'], package['filename'])
+		tag = "<script type=\"text/javascript\" src=\"%s%s\"></script>" % (cdn_url, path)
+		self.view.insert(self.edit, self.view.sel()[0].begin(), tag)
+
+class CdnjsApiCall(threading.Thread):
+    def __init__(self, timeout):
+        self.timeout = timeout
+        self.result = None
+        threading.Thread.__init__(self)
+
+    def run(self):
+        try:
+            request = urllib2.Request('http://www.cdnjs.com/packages.json', headers={"User-Agent": "Sublime cdnjs"})
+            http_file = urllib2.urlopen(request, timeout=self.timeout)
+            self.result = http_file.read()
+            return json.loads(self.result)['packages'][:-1]
+
+        except (urllib2.HTTPError) as (e):
+            err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+        except (urllib2.URLError) as (e):
+            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+
+        sublime.error_message(err)
+        self.result = False
+        return False
