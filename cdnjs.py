4e78dfb9936712cf91c35cb475d914bbd2abef7d
Dafydd Francis <daveordref@gmail.com>
2013-03-12 21:43:09 +0000

    port to python3 so it can work on Sublime Text 3

--- cdnjs.py
+++ cdnjs.py
@@ -1,70 +1,85 @@
 import sublime
 import sublime_plugin
-import urllib2
 import threading
 import json
 import os
+try:
+    from urllib.request import Request
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+    from urllib.error import URLError
+except:
+    from urllib2 import Request
+    from urllib2 import urlopen
+    from urllib2 import HTTPError
+    from urllib2 import URLError
 
 
 class CdnjsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        CdnjsApiCall(self.view, edit, 30).start()
+        CdnjsApiCall(self.view, 30).start()
 
+class CdnjsPlaceTextCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **args):
+        self.view.insert(edit, self.view.sel()[0].begin(), args["tag"])
 
 class CdnjsApiCall(threading.Thread):
-    def __init__(self, view, edit, timeout):
+    CDN_URL = "//cdnjs.cloudflare.com/ajax/libs/"
+    PACKAGES_URL = 'http://www.cdnjs.com/packages.json'
+
+    def __init__(self, view, timeout):
         self.view = view
-        self.edit = edit
         self.timeout = timeout
         threading.Thread.__init__(self)
 
     def run(self):
         try:
-            request = urllib2.Request('http://www.cdnjs.com/packages.json',
+            request = Request(self.PACKAGES_URL,
                 headers={"User-Agent": "Sublime cdnjs"}
             )
-            http_file = urllib2.urlopen(request, timeout=self.timeout)
-            result = http_file.read()
+            http_file = urlopen(request, timeout=self.timeout)
+            result = http_file.read().decode('utf-8')
 
             self.packages = json.loads(result)['packages'][:-1]
-            package_list = [[x['name'], x['description']] for x in self.packages]
-
-            def show_quick_panel():
-                if not package_list:
-                    sublime.error_message(('%s: There are no packages available'))
-                    return
-                self.view.window().show_quick_panel(package_list, self.insert_tag)
+            self.package_list = [[x['name'], x['description']] for x in self.packages]
 
             # show_quick_panel must execute on the main thread. This timeout will make it so
-            sublime.set_timeout(show_quick_panel, 10)
-        except (urllib2.HTTPError) as (e):
-            sublime.error_message('%s: HTTP error %s contacting API' % (__name__, str(e.code)))
-        except (urllib2.URLError) as (e):
-            sublime.error_message('%s: URL error %s contacting API' % (__name__, str(e.reason)))
+            sublime.set_timeout(self.show_quick_panel, 10)
+        except HTTPError as e:
+            error_str = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+            sublime.error_message(error_str)
+        except URLError as e:
+            error_str = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+            sublime.error_message(error_str)
+
+    def show_quick_panel(self):
+        if not self.package_list:
+            sublime.error_message(('%s: There are no packages available'))
+            return
+        self.view.window().show_quick_panel(self.package_list, self.insert_tag)
 
     def insert_tag(self, index):
         if index == -1:
             return
         pkg = self.packages[index]
-        cdn_url = "//cdnjs.cloudflare.com/ajax/libs/"
-        path = cdn_url + "%s/%s/%s" % (pkg['name'], pkg['version'], pkg['filename'])
-        
+        path = self.CDN_URL + "%(name)s/%(version)s/%(filename)s" % pkg
+
         markup = os.path.splitext(self.view.file_name() or "")[1]
 
         tag_type = os.path.splitext(pkg['filename'])[1]
         is_css = tag_type == '.css'
 
         if markup   == '.slim':
-            if is_css:            tag = "link rel=\"stylesheet\" href=\"%s\"" % path
+            if is_css:            tag = "link href=\"%s\"" % path
             else:                 tag = "script src=\"%s\"" % path
         elif markup == '.haml':
-            if is_css:            tag = "%%link{:rel=>\"stylesheet\",:href=>\"%s\"}" % path
+            if is_css:            tag = "%%link{:href=>\"%s\"}" % path
             else:                 tag = "%%script{:src=>\"%s\"}" % path
         elif markup == '.jade':
-            if is_css:            tag = "link(rel=\"stylesheet\",href=\"%s\")" % path
+            if is_css:            tag = "link(href=\"%s\")" % path
             else:                 tag = "script(src=\"%s\")" % path
         else:
-            if is_css:            tag = "<link rel=\"stylesheet\" href=\"%s\" />" % path
+            if is_css:            tag = "<link href=\"%s\">" % path
             else:                 tag = "<script src=\"%s\"></script>" % path
 
-        self.view.insert(self.edit, self.view.sel()[0].begin(), tag)
+        self.view.run_command('cdnjs_place_text', {"tag": tag})
